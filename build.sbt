d6980aff0e38fc83855d20f187e6b267a051b1b4
Yesid Botero <yesidboterom@gmail.com>
2017-10-12 22:08:25 -0500

    construyendo test multijvm

--- build.sbt
+++ build.sbt
@@ -1,12 +1,21 @@
-name := "akka-distributed-data-school"
+val akkaVersion = "2.5.6"
 
-version := "1.0"
-
-scalaVersion := "2.12.3"
-
-libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-actor" % "2.5.6",
-  "com.typesafe.akka" %% "akka-cluster" % "2.5.6",
-  "com.typesafe.akka" %% "akka-distributed-data" % "2.5.6"
-)
-        
+val `akka-sample-distributed-data-scala` = project
+  .in(file("."))
+  .settings(
+    organization := "com.typesafe.akka.samples",
+    scalaVersion := "2.12.3",
+    scalacOptions in Compile ++= Seq("-deprecation", "-feature", "-unchecked", "-Xlog-reflective-calls", "-Xlint"),
+    javacOptions in Compile ++= Seq("-Xlint:unchecked", "-Xlint:deprecation"),
+    javaOptions in run ++= Seq("-Xms128m", "-Xmx1024m"),
+    libraryDependencies ++= Seq(
+      "com.typesafe.akka" %% "akka-actor" % akkaVersion,
+      "com.typesafe.akka" %% "akka-remote" % akkaVersion,
+      "com.typesafe.akka" %% "akka-cluter" % akkaVersion,
+      "com.typesafe.akka" %% "akka-distributed-data" % akkaVersion,
+      "com.typesafe.akka" %% "akka-multi-node-testkit" % akkaVersion,
+      "org.scalatest" %% "scalatest" % "3.0.1" % Test),
+    fork in run := true,
+    // disable parallel tests
+    parallelExecution in Test := false
+  )
---
+++ src/main/scala/Greeter.scala
@@ -0,0 +1,39 @@
+import akka.actor.{Actor, ActorRef}
+import akka.cluster.Cluster
+import akka.cluster.ddata.{DistributedData, ORSet, ORSetKey}
+
+import scala.concurrent.duration._
+
+object Greeter {
+  import akka.cluster.ddata.Replicator._
+
+  case class Greet(greet: String)
+
+  private val readMajority = ReadMajority(3.seconds)
+  private val writeMajority = WriteMajority(3.seconds)
+}
+
+class Greeter extends Actor {
+  import Greeter._
+  import akka.cluster.ddata.Replicator._
+
+  //Se obtiene el ActorRef del Replicator asociado al Cluster
+  val replicator = DistributedData(context.system).replicator
+  //Se define la llave con la que se accederá a los datos
+  val DataKey = ORSetKey[String]("llave")
+  implicit val cluster = Cluster(context.system)
+  //Si se quiere que el actor recibe un mensaje cada vez que una actualización sea realizada
+  //replicator ! Subscribe(DataKey, self)
+
+  override def receive: Receive = {
+    case Greet(greet) =>
+      replicator ! Update(DataKey, ORSet.empty[String], writeMajority, Some(sender()))(_ + " ")
+    case u @ UpdateSuccess(DataKey, Some(originalSender: ActorRef)) =>
+      replicator ! Get(DataKey, readMajority, Some(originalSender))
+    case g @ GetSuccess(DataKey, Some(originalSender: ActorRef)) =>
+      val saludos = g.get(DataKey).elements
+      originalSender ! saludos
+  }
+}
+
+
